bcb0a25bd2114856a321f2a1cd225868b246f76a
Hemanth vasu <hemanthvasu41468@gmail.com>
2026-04-26 00:22:04 +0530

    day 2 Second largest element

---
+++ Second_Largest_Element.java
@@ -0,0 +1,30 @@
+public class Second_Largest_Element {
+    public static void main(String[] args) {
+
+        int[] arr = {10,5,20,8};
+
+        int result = solution(arr);
+
+        System.out.println("Second Largest: " + result);
+    }
+
+    public static int solution(int[] arr) {
+        int max=Integer.MIN_VALUE;
+        int secondmax=Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]>max)
+            {
+                secondmax=max;
+                max=arr[i];
+            }
+            else if(arr[i]>secondmax && arr[i]!=max)
+            {
+                secondmax=arr[i];
+            }  
+        }
+
+        return secondmax;
+    }
+}
